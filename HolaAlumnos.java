fc5782f458097bddde0b254c04f7a2090736d1d6
Sandra <karwowskasandra@gmail.com>
2024-10-14 12:25:51 +0200

    añadida linea con mi nombre completo

--- HolaAlumnos.java
+++ HolaAlumnos.java
@@ -1,6 +1,7 @@
 public class HolaAlumnos {
  
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+                System.out.println("Sandra Karwowska");
 		System.out.println("Hola alumnos de DAM1!!");
 	}
  
